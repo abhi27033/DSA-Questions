7befe4dcb338e6fee0fd74f844b4f9d04228a963
abhi27033 <117388925+abhi27033@users.noreply.github.com>
2023-04-23 15:33:39 +0530

    Added solution - LeetHub

---
+++ Minimum Number - GFG/minimum-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int minimumNumber(int n,vector<int> &arr){
+       int flg=0,ans=INT_MAX;
+       for(auto it:arr)
+       {
+           if(it%2==1)
+           {
+               flg=1;
+               break;
+           }
+           ans=min(ans,it);
+       }
+       if(flg)
+       return flg;
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.minimumNumber(n,arr)<<endl;
+    }
+}
+// } Driver Code Ends
