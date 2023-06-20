2acd469032f83790e1d94d8075b314e525e47c0c
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2023-06-20 23:48:54 +0530

    Added solution - LeetHub

--- Matchsticks game - GFG/matchsticks-game.cpp
+++ Matchsticks game - GFG/matchsticks-game.cpp
@@ -14,12 +14,7 @@ class Solution {
         int s=N/5;
         N-=s;
         int ans=N%4;
-        
         return ans==0?4:ans;
-        // 6 1
-        // 7 2
-        // 8 3
-        // 9 4
     }
 };
 
