baeac0aea206eaa1ab260445ecc07070eed7f62e
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2023-07-19 00:57:26 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,26 +10,44 @@ using namespace std;
 
 class Solution{
   public:
-  int solve(string& a,int i,int j,vector<vector<int>>& dp)
-  {
-      if(i>j||j<0||i>=a.size())
-      return 0;
-      if(i==j)
-      return 1;
-      if(dp[i][j]!=-1)
-     return dp[i][j];
-      int ans=0;
-      if(a[i]==a[j])
-      ans=2+solve(a,i+1,j-1,dp);
-      else
-      ans=max(solve(a,i+1,j,dp),solve(a,i,j-1,dp));
-      return dp[i][j]=ans;
-  }
+//   int solve(string& a,int i,int j,vector<vector<int>>& dp)
+//   {
+//       if(i>j||j<0||i>=a.size())
+//       return 0;
+//       if(i==j)
+//       return 1;
+//       if(dp[i][j]!=-1)
+//      return dp[i][j];
+//       int ans=0;
+//       if(a[i]==a[j])
+//       ans=2+solve(a,i+1,j-1,dp);
+//       else
+//       ans=max(solve(a,i+1,j,dp),solve(a,i,j-1,dp));
+//       return dp[i][j]=ans;
+//   }
     int longestPalinSubseq(string A) {
         //code here
         int n=A.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(A,0,n-1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+        if(i==j)
+        dp[i][j]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            // dp[i][i]=1;
+            for(int j=0;j<n;j++)
+            {
+                if(j>i)
+                {
+                if(A[i]==A[j])
+                dp[i][j]=2+dp[i+1][j-1];
+                else
+                dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
 
