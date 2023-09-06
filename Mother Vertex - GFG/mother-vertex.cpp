efdb46a7f162ab6850b81093d7c5958d1f76cddc
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2023-09-06 21:11:34 +0530

    Added solution - LeetHub

---
+++ Mother Vertex - GFG/mother-vertex.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution 
+{
+    public:
+    //Function to find a Mother Vertex in the Graph.
+    void solve(vector<int>adj[],int i,vector<int>&vis,stack<int>&s,int& c)
+    {
+        vis[i]=1;
+        c++;
+        for(auto it:adj[i])
+        {
+            if(vis[it]==-1)
+            {
+            solve(adj,it,vis,s,c);
+            }
+        }
+        s.push(i);
+    }
+	int findMotherVertex(int V, vector<int>adj[])
+	{
+	    // Code here
+	    int flg=-1;
+	    vector<int> vis(V,-1);
+	    stack<int> s;
+	    for(int i=0;i<V;i++)
+	    {
+	        if(vis[i]==-1)
+	        solve(adj,i,vis,s,flg);
+	    }
+	    int n=s.top();
+	    fill(vis.begin(),vis.end(),-1);
+	    int c=0;
+	    solve(adj,n,vis,s,c);
+	    return c==V?n:-1;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+		}
+		Solution obj;
+		int ans = obj.findMotherVertex(V, adj);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
