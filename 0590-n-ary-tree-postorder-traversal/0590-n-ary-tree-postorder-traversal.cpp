dab9835c1619824f37960ecc627d51297c61d658
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-03-01 15:52:45 +0530

    Time: 17 ms (41.51%), Space: 15 MB (55.24%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void solve(Node* r,vector<int>& ans)
+    {
+        if(!r)
+            return;
+        for(auto it:(r->children))
+            solve(it,ans);
+        ans.push_back(r->val);
+    }
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
