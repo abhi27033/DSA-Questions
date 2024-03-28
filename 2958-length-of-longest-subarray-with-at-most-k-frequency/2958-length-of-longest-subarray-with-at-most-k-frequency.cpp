daeaa36766308b1c97d89f4b32e9f73c331b92a8
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-03-28 11:16:12 +0530

    Time: 240 ms (29.15%), Space: 150 MB (11.79%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             mp[nums[i]]++;
-            while(j<=i&&mp[nums[i]]>k)
+            while(mp[nums[i]]>k)
             {
                 mp[nums[j]]--;
                 if(mp[nums[j]]==0)
