4d83fe4f92963466892d4f46a80ab02ac0d87cbd
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-03-28 11:16:03 +0530

    Time: 219 ms (46.83%), Space: 150.3 MB (7.75%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int j=0,ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+            while(j<=i&&mp[nums[i]]>k)
+            {
+                mp[nums[j]]--;
+                if(mp[nums[j]]==0)
+                    mp.erase(nums[j]);
+                j++;
+            }
+            ans=max(ans,i-j+1);
+        }
+        return ans;
+    }
+};
