d14f26cbf08fbf28f382920bfa8c5f4fd435fd03
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-04-07 09:53:18 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (40.00%) - LeetHub

--- 3106-lexicographically-smallest-string-after-operations-with-constraint/3106-lexicographically-smallest-string-after-operations-with-constraint.cpp
+++ 3106-lexicographically-smallest-string-after-operations-with-constraint/3106-lexicographically-smallest-string-after-operations-with-constraint.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    string getSmallestString(string s, int k) {
-        string ans;
-        for(int i=0;i<s.size();i++)
+    bool solve(int i,string &ans,string &s,int k)
+    {
+        if(i==s.size()){
+            return true;
+        }
+        for(char c='a';c<='z';c++)
         {
-            if(s[i]=='a')
-                continue;
-            int t=s[i]-'a';
-            if(k>=min(26-t,t))
-            {
-                k-=min(26-t,t);
-                s[i]='a';
-            }
-            else
-            {
-                if(t>=k)
-                    s[i]-=k;
-               return s;
+            int a=min(s[i]-c,((26-(s[i]-'a'))+(c-'a')));
+            if(a<=k){
+                ans.push_back(c);
+                if(solve(i+1,ans,s,k-a)) return true;
+                ans.pop_back();
             }
         }
-        return s;
+        return false;
+    }
+    string getSmallestString(string s, int k) {
+        string ans="";
+        if(k==0) return s;
+        solve(0,ans,s,k);
+        return ans;
     }
 };
