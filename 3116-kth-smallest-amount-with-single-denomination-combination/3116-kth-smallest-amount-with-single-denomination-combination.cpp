5ab0444569fc86797feba315e886227cad3cfca4
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-04-14 14:00:13 +0530

    Time: 110 ms (6.67%), Space: 19.5 MB (86.67%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -0,0 +1,64 @@
+
+class Solution {
+public:
+    long long gcd(long long a, long long b) {
+        while (b != 0) {
+            long long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    long long lcm(long long a, long long b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    long long countS(vector<int>& coins, long long amount) {
+        long long count = 0;
+//         for (long long it : coins) {
+//             count += amount / it;
+//         }
+// cout<<count<<endl;
+        int n = coins.size();
+        for (long long mask = 1; mask < (1 << n); ++mask) {
+            long long f = 1;
+            long long cnt = 0;
+            for (long long i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    f = lcm(f, coins[i]);
+                    ++cnt;
+                }
+            }
+            if (cnt %2==1) {
+                // cout<<f<<" "<<(amount / f)<<"-- ";
+                count =count+ (amount / f);
+            }
+            else
+            {
+                count =count- (amount / f);
+            }
+        }
+// cout<<endl;
+        return count;
+    }
+
+    long long findKthSmallest(vector<int>& coins, long long k) {
+        long long left = 1;
+        long long right = LONG_MAX, ans = -1;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long count = countS(coins, mid);
+            // cout<<count<<" " <<mid<<endl;
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                ans=right;
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+};
