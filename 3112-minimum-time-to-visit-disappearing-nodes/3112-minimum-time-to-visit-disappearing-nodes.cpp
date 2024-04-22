968f9ca1b418c508682aa7f3b75bea675bd47cf7
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-04-22 23:17:44 +0530

    Time: 407 ms (35.05%), Space: 176.8 MB (39.77%) - LeetHub

--- 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.cpp
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     vector<int> minimumTime(int n, vector<vector<int>>& edges, vector<int>& disappear) {
-        unordered_map<int, unordered_map<int, int>> adj;
-        vector<int> dis(n, INT_MAX);
-        
-        // Constructing the adjacency list and keeping track of the minimum weight for each edge
-        for (int i = 0; i < edges.size(); ++i) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-            
-            if (adj[u].find(v) == adj[u].end() || adj[u][v] > w) {
-                adj[u][v] = w;
-                adj[v][u] = w; // Assuming undirected graph
-            }
+        vector<int> dis(n,-1);
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:edges)
+        {
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
-        
-        // Dijkstra's algorithm
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, 0});
-        dis[0] = 0;
-        
-        while (!pq.empty()) {
-            int d = pq.top().first;
-            int u = pq.top().second;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<int> di(n,INT_MAX);
+        di[0]=0;
+        pq.push({0,0});
+        while(pq.size())
+        {
+            auto it=pq.top();
             pq.pop();
-            
-            if (d > dis[u]) continue;
-            
-            for (auto& [v, w] : adj[u]) {
-                if (dis[v] > dis[u] + w && disappear[v] > dis[u] + w) {
-                    dis[v] = dis[u] + w;
-                    pq.push({dis[v], v});
+            int d=it.first;
+            int node=it.second;
+            if(disappear[node]<=d||di[node]<d)continue;
+            for(auto itt:adj[node])
+            {
+                if(disappear[itt.first]>d+itt.second&&di[itt.first]>di[node]+itt.second)
+                {
+                   di[itt.first]=di[node]+itt.second;
+                   pq.push({di[itt.first],itt.first});
                 }
             }
         }
-        
-        // Handling nodes that cannot be reached within the given time
-        for (int i = 0; i < n; ++i) {
-            if (dis[i] >= disappear[i]) dis[i] = -1;
-        }
-        
-        return dis;
+        for(int i=0;i<n;i++)
+        di[i]=di[i]>=INT_MAX?-1:di[i];
+        return di;
     }
 };
