a20f57df3c67eb24b88c491c332a6c61e7ef6569
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-04-26 00:30:45 +0530

    Time: 27 ms (98.32%), Space: 11.5 MB (62.18%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    // int solve(string& s,int idx,int pidx,int& k)
-    // {
-    //     if(idx>=s.size())return 0;
-    //     if(dp[idx][pidx+1]!=-1)return dp[idx][pidx+1];
-    //     int a=0,b=0;
-    //     if(pidx==-1||abs(s[pidx]-s[idx])<=k)
-    //         a=1+solve(s,idx+1,idx,k);
-    //     b=solve(s,idx+1,pidx,k);
-    //     return dp[idx][pidx+1]=max(a,b);
-    // }
     int longestIdealString(string s, int k) {
         vector<int> mp(26,0);
         for(int i=0;i<s.size();i++)
