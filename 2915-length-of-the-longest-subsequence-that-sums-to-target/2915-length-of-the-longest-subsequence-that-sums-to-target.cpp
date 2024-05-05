7631a22d39471802de617a35c1a274fb0dae4efd
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-05-05 13:27:43 +0530

    Time: 192 ms (66.98%), Space: 155.8 MB (38.68%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -12,8 +12,21 @@ public:
     }
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
         int n=nums.size();
-        dp.resize(n+1,vector<int>(target+1,-1));
-        int x=solve(nums,target,0);
+        dp.resize(n+1,vector<int>(target+1,-1e6));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int t=0;t<=target;t++)
+            {
+                int pick=-1e6,npick=-1e6;
+                if(t-nums[i]>=0)
+                    pick=1+dp[i+1][t-nums[i]];
+                npick=dp[i+1][t];
+                dp[i][t]=max(pick,npick);
+            }
+        }
+        int x=dp[0][target];
         return x<0?-1:x;
     }
 };
