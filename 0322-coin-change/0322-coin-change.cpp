bf717858d9c1514140fd624831ca60119be2ae4e
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-05-05 12:16:15 +0530

    Time: 57 ms (16.27%), Space: 51 MB (13.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(vector<int>& ar,int idx,int amt)
-    {
-        if(idx>=ar.size()||amt<0)return 1e7;
-        if(amt==0)return 0;
-        if(dp[idx][amt]!=-1)return dp[idx][amt];
-        int pick=1+solve(ar,idx,amt-ar[idx]);
-        int npick=solve(ar,idx+1,amt);
-        return dp[idx][amt]=min(pick,npick);        
-    }
+    // int solve(vector<int>& ar,int idx,int amt)
+    // {
+    //     if(idx>=ar.size()||amt<0)return 1e7;
+    //     if(amt==0)return 0;
+    //     if(dp[idx][amt]!=-1)return dp[idx][amt];
+    //     int pick=1+solve(ar,idx,amt-ar[idx]);
+    //     int npick=solve(ar,idx+1,amt);
+    //     return dp[idx][amt]=min(pick,npick);        
+    // }
     int coinChange(vector<int>& coins, int amount) {
         dp.resize(coins.size()+1,vector<int>(amount+1,-1));
         // int ans= solve(coins,0,amount);
