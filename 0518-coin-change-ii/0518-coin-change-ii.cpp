31ca22abcb7735c7c8d5f978bda0786fedd66d2e
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2024-05-05 12:27:25 +0530

    Time: 19 ms (24.29%), Space: 19.9 MB (42.88%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -12,7 +12,22 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        dp.resize(n+1,vector<int>(amount+1,-1));
-        return solve(coins,0,amount);
+        dp.resize(n+1,vector<int>(amount+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=1;
+        // return solve(coins,0,amount);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int amt=0;amt<=amount;amt++)
+            {
+                int pick=0,npick=0;
+                if(amt-coins[i]>=0)
+                pick=dp[i][amt-coins[i]];
+                if(i+1<=n)
+                npick=dp[i+1][amt];
+                dp[i][amt]=pick+npick;
+            }
+        }
+        return dp[0][amount];
     }
 };
