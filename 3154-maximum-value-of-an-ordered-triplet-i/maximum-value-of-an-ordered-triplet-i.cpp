7a8881a3813895ce017e9e13acec249a894acebc
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-04-02 23:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (66.87%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        vector<int> leftmax(nums.size()),rightmax(nums.size());
-        int n=nums.size();
-        leftmax[0]=nums[0];
-        rightmax[n-1]=nums[n-1];
-        for(int i=1;i<nums.size();i++)leftmax[i]=max(leftmax[i-1],nums[i]);
-        for(int i=n-2;i>=0;i--)rightmax[i]=max(rightmax[i+1],nums[i]);
-        long long ans=0;
-        for(int i=1;i<n-1;i++)
-        ans=max(ans,1LL*(1LL*(leftmax[i-1]-nums[i])*rightmax[i+1]));
+        // vector<int> leftmax(nums.size()),rightmax(nums.size());
+        // int n=nums.size();
+        // leftmax[0]=nums[0];
+        // rightmax[n-1]=nums[n-1];
+        // for(int i=1;i<nums.size();i++)leftmax[i]=max(leftmax[i-1],nums[i]);
+        // for(int i=n-2;i>=0;i--)rightmax[i]=max(rightmax[i+1],nums[i]);
+        // long long ans=0;
+        // for(int i=1;i<n-1;i++)
+        // ans=max(ans,1LL*(1LL*(leftmax[i-1]-nums[i])*rightmax[i+1]));
+        // return ans;
+        const int n=nums.size();
+        long long L=nums[0], maxD=L-nums[1], ans=max(0LL, maxD*nums[2]);
+        for(int k=3; k<n; k++){
+            L=max(L, (long long)nums[k-2]);
+            maxD=max(maxD, L-nums[k-1]);
+            ans=max(ans, maxD*nums[k]);
+        }
         return ans;
     }
 };
