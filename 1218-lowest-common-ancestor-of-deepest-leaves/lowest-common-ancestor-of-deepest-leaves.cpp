2ad53edce10f43c65f74e43c79b2217e7671cea1
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-04-05 00:04:28 +0530

    Time: 7 ms (8.22%) | Memory: 23.7 MB (12.58%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,50 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        vector<vector<TreeNode*>> depth;
         queue<TreeNode*> q;
         unordered_map<TreeNode*, TreeNode*> par;
+        vector<TreeNode*> lastLevel;
         q.push(root);
-        while(q.size()){
-            int sz=q.size();
-            vector<TreeNode*> temp;
-            while(sz--){
-                TreeNode* t=q.front();
-                temp.push_back(t);
+        while (!q.empty()) {
+            int sz = q.size();
+            lastLevel.clear();
+            while (sz--) {
+                TreeNode* t = q.front();
                 q.pop();
-                if(t->left)
-                {
-                q.push(t->left);
-                par[t->left]=t;
+                lastLevel.push_back(t);
+                if (t->left) {
+                    q.push(t->left);
+                    par[t->left] = t;
                 }
-                if(t->right)
-                {
-                q.push(t->right);
-                par[t->right]=t;
+                if (t->right) {
+                    q.push(t->right);
+                    par[t->right] = t;
                 }
             }
-            depth.push_back(temp);
         }
-        unordered_map<TreeNode*,int> mark;
-        for(auto it:depth.back())
-        mark[it]=1;
-        while(mark.size()>1){
-            unordered_map<TreeNode*,int> nm;
-            for(auto it:mark)
-            nm[par[it.first]]=1;
-            mark=nm;
+
+        unordered_map<TreeNode*, int> mark;
+        for (auto it : lastLevel) 
+            mark[it] = 1;
+
+        while (mark.size() > 1) {
+            unordered_map<TreeNode*, int> nm;
+            for (auto it : mark)
+                nm[par[it.first]] = 1;
+            mark = nm;
         }
+
         return mark.begin()->first;
     }
 };
