3eba999b1138ac366b11b90b6950f0d9c86d6cba
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-04-11 01:21:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (65.59%) - LeetSync

--- 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
+++ 3243-count-the-number-of-powerful-integers/count-the-number-of-powerful-integers.cpp
@@ -18,9 +18,6 @@ long long solve(string& ub,string s,int l,int z){
     if(ub.substr(left,s.size())>=s) ans++;
     return ans;
 }
-// 3
-// 121->ub
-// _12
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
         string fin=to_string(finish);
         // cout<<fin<<endl;
@@ -31,45 +28,3 @@ long long solve(string& ub,string s,int l,int z){
         return lh-ll;
     }
 };
-
-// start --> finish
-// <=finish--->x
-// <=(start-1)---->y
-// x-y--
-
-//   4
-//   3 4 3 1  _ _ _
-//   0 0 0 0->3*5*5*5
-//   1 1 1 1 
-//   2 2 2 2
-//     3 3 3 
-//     4 4 4
-// f[i]-1,
-//   3  
-//   0 0 0 0->1*4*5*5
-//     1 1 1
-//     2 2 2
-//     3 3 3
-//       4 4
-      
-//   0 0 0 0->1*1*3*5
-//       1 1
-//       2 2
-//         3
-//         4
-      
-//   0 0 0 0->1
-    
-// limit  
-// 3 4 3 1  _ _ _
-// 0 0 0 
-// 1 1 1
-// 2 2 2
-// 3 3 3
-//   4 4
-
-// finish = 3 4 5 2, limit = 5=8
-//              1 2
-//          3 5
-//              5 2
-//          3 4
