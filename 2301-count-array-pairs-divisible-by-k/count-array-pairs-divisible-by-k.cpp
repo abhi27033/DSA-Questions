831246f591e9e5fdf3d6b1cd899725b73da28bf2
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-04-17 23:24:27 +0530

    Time: 482 ms (11.88%) | Memory: 107 MB (10.63%) - LeetSync

--- 2301-count-array-pairs-divisible-by-k/count-array-pairs-divisible-by-k.cpp
+++ 2301-count-array-pairs-divisible-by-k/count-array-pairs-divisible-by-k.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     long long countPairs(vector<int>& nums, int k) {
+        // HARD --> https://leetcode.com/problems/count-array-pairs-divisible-by-k/
         unordered_map<int,int> factors;
+        int zeroCount=0;
         long long ans=0;
         for(auto it:nums){
             int gcdd=__gcd(it,k);
             int left=k/gcdd;
             if(factors.find(left)!=factors.end())
             ans+=factors[left];
+            ans+=zeroCount;
+            if(it==0)zeroCount++;
             for(int i=1;i*i<=it;i++){
                 if(it%i==0)
                 {
