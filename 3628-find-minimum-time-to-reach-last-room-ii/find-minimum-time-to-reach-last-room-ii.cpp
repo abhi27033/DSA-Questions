2b3416c49972b3ac8fe683ced3d392032c95ea27
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-05-08 10:19:27 +0530

    Time: 1563 ms (8.43%) | Memory: 191.7 MB (20.25%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        set<pair<int,int>> vis;
+        vis.insert({0,0});
+        int ans=0;
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0,0});
+        vector<int> dx={1,0,-1,0};
+        vector<int> dy={0,1,0,-1};
+        while(pq.size()){
+            int cost=pq.top()[0];
+            int i=pq.top()[1];
+            int j=pq.top()[2];
+            int q=pq.top()[3];
+            pq.pop();
+            if(i==moveTime.size()-1&&j==moveTime[0].size()-1)return cost;
+            for(int k=0;k<4;k++){
+                int ii=i+dx[k];
+                int jj=j+dy[k];
+                if(ii>=0&&jj>=0&&ii<moveTime.size()&&jj<moveTime[0].size()&&vis.find({ii,jj})==vis.end()){
+                    pq.push({max(moveTime[ii][jj],cost)+1+q,ii,jj,(q+1)%2});
+                    vis.insert({ii,jj});
+                }
+            }
+        }
+        return -1;
+    }
+};
