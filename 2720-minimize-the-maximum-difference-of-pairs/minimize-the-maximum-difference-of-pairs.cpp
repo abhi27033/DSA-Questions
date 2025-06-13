95433f4c12551c309884106a604cbf22739570c1
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-06-13 23:41:55 +0530

    Time: 24 ms (68.64%) | Memory: 86.6 MB (99.12%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(),nums.end());
+        int l=0,h=nums.back()-nums[0],ans=0;
+        if(p==0)return 0;
+        while(l<=h){
+            int m=l+(h-l)/2,k=0;
+            for(int i=1;i<nums.size();i++){
+                if(m>=nums[i]-nums[i-1]){
+                    i++;
+                    k++;
+                }
+            }
+            if(k>=p){
+                h=m-1;
+                // if(k==p)
+                // ans=max(ans,m);
+            }
+            else l=m+1;
+        }
+        return h+1;
+    }
+};
