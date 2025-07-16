f43626dd293bbeaeacb6b738c24a781a5e5be231
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-07-16 21:50:07 +0530

    Time: 3 ms (73.24%) | Memory: 96 MB (82.65%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
-vector<vector<vector<int>>> dp;
-    int solve(vector<int>& nums,int i,int c,int cs,int& s){
-        if(i>=nums.size()){
-            if(c!=0)return 0;return -1e9;
-        }
-        if(dp[i][c][cs]!=-1)return dp[i][c][cs];
-        int npick=solve(nums,i+1,c,cs,s);
-        int pick=-1e9;
-        if(c==0)
-        pick=1+solve(nums,i+1,c+1,nums[i]%2,s);
-        else if((cs+nums[i])%2==s)
-        pick=1+solve(nums,i+1,1,nums[i]%2,s);
-        return dp[i][c][cs]=max(pick,npick);
-    }
     int maximumLength(vector<int>& nums) {
-        int n=nums.size();
-        dp.resize(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        int a=0,b=1,c=2;
-        int ans=0;
-        ans=max(ans,solve(nums,0,0,0,a));
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0][0]=-1;
-            dp[i][0][1]=-1;
-            dp[i][0][2]=-1;
-            dp[i][1][0]=-1;
-            dp[i][1][1]=-1;
-            dp[i][1][2]=-1;
+        int str1=0,ans=0,c1=0,str2=1,c2=0,c3=0,c4=0;
+        for(auto it:nums){
+            if(it%2==str1){
+                c1++;
+                ans=max(ans,c1);
+                str1=1-str1;
+            }
+            if(it%2==str2){
+                c2++;
+                ans=max(ans,c2);
+                str2=1-str2;
+            }
+            if(it%2==1)
+            {
+                c3++;
+                ans=max(ans,c3);
+            }
+            else
+            {
+                c4++;
+                ans=max(ans,c4);
+            }
         }
-        ans=max(ans,solve(nums,0,0,0,b));
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0][0]=-1;
-            dp[i][0][1]=-1;
-            dp[i][0][2]=-1;
-            dp[i][1][0]=-1;
-            dp[i][1][1]=-1;
-            dp[i][1][2]=-1;
-        }
-        ans=max(ans,solve(nums,0,0,0,c));
         return ans;
+        
     }
 };
