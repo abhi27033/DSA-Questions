59b0930379ace656fdf2d30a0596f7749451688a
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-07-30 22:40:52 +0530

    Time: 18 ms (67.67%) | Memory: 35.5 MB (9.29%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+vector<int> dp;
+bool solve(vector<int>& nums,int i,int mask,int k,int cs,int& rs){
+    if(k==0)return cs==0&&(mask==(1<<nums.size())-1);
+    if(cs>rs)return false;
+    if(dp[mask]!=-1)return dp[mask];
+    if(cs==rs)return dp[mask]=solve(nums,0,mask,k-1,0,rs);
+    for(int st=i;st<nums.size();st++){
+        if(mask>>st&1)continue;
+        if(solve(nums,st+1,mask|(1<<st),k,cs+nums[st],rs))return dp[mask]=true;
+    }
+    return dp[mask]=false;
+    
+}
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int s=0;
+        for(auto it:nums)s+=it;
+        if(s%k!=0)return false;
+        sort(nums.begin(),nums.end());
+        int reqsum=s/k,n=nums.size();
+        dp.resize(1<<(n+1),-1);
+        return solve(nums,0,0,k,0,reqsum);
+    }
+};
