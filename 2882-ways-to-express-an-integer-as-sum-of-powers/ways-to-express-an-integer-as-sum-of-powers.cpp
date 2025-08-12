630abdb47be6c2034873fede2bcdcf6a292d37a5
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-08-12 22:42:28 +0530

    Time: 223 ms (49.24%) | Memory: 274.6 MB (23.31%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int mod=1e9+7;
+int pow(int b, int e){
+    int t=1;
+    while(e>=1){
+        if(e&1)
+        {
+            t=(t%mod*b%mod)%mod;
+            e--;
+        }
+        else
+        {
+            b=(b%mod*b%mod)%mod;
+            e/=2;
+        }
+    }
+    return t%mod;
+}
+vector<vector<int>> ar;
+int solve(int n,int i,int& x){
+    if(n==0)return 1;
+    if(n<0||pow(i,x)>n)return 0;
+    if(ar[n][i]!=-1)return ar[n][i]%mod;
+    return ar[n][i]=solve(n,i+1,x)%mod+solve(n-pow(i,x),i+1,x)%mod;
+}
+    int numberOfWays(int n, int x) {
+        ar.resize(n+1,vector<int>(n+1,-1));
+        return solve(n,1,x)%mod;
+    }
+};
