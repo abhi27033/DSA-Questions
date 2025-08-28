937ce099670d365451428a59b497ee11b4d41787
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-08-29 00:06:15 +0530

    Time: 10 ms (55.76%) | Memory: 44.9 MB (40.33%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
+        int n=grid.size(),ii,jj;
         for(int i=0;i<n+n-1;i++){
             if(i>=n){
-                int ii=0;
-                int jj=i-n+1;
-                vector<int> q;
-                int ti=ii,tj=jj;
-                while(ti<n&&tj<n)
-                    q.push_back(grid[ti++][tj++]);
-                ti=ii,tj=jj;
-                int f=0;
-                sort(q.begin(),q.end());
-                while(ti<n&&tj<n)
-                    grid[ti++][tj++]=q[f++];
+                ii=0;
+                jj=i-n+1;
             }
             else{
-                int ii=n-i-1;
-                int jj=0;
-                vector<int> q;
-                int ti=ii,tj=jj;
-                while(ti<n&&tj<n)
-                    q.push_back(grid[ti++][tj++]);
-                ti=ii,tj=jj;
-                int f=0;
-                sort(q.rbegin(),q.rend());
-                while(ti<n&&tj<n)
-                    grid[ti++][tj++]=q[f++];
+                ii=n-i-1;
+                jj=0;
             }
+            vector<int> q;
+            int ti=ii,tj=jj;
+            while(ti<n&&tj<n)
+                q.push_back(grid[ti++][tj++]);
+            ti=ii,tj=jj;
+            int f=0;
+            if(i>=n)
+            sort(q.begin(),q.end());
+            else
+            sort(q.rbegin(),q.rend());
+            while(ti<n&&tj<n)
+                grid[ti++][tj++]=q[f++];
         }
         return grid;
     }
