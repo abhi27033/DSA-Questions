9d8fbf49b2835a635c459f6ebecff1cd587e569e
Abhishek Singh <117388925+abhi27033@users.noreply.github.com>
2025-09-05 19:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.95%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -4,21 +4,13 @@ public:
     int makeTheIntegerZero(int num1, int num2) {
         for(int i=0;i<=40;i++){
             long long target=num1-1LL*num2*i;
-            long long ans=0,q=0;
-            if(target<=0)break;
+            long long ans=0;
+            if(target<i)break;
             while(target>0){
                 ans+=(target&1);
-                q++;
                 target>>=1;
             }
-            if(ans<=i)
-            {
-                if(i-ans){
-                    if(q>1)return i;
-                }
-                else
-                return i;
-            }
+            if(ans<=i)return i;
         }
         return -1;
     }
